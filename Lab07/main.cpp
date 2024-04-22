9558c0f1ec0ecfd49b53272d5254e15398e6216b
awds0318 <brianlin910318@gmail.com>
2024-04-22 16:15:21 +0800

    update Lab07

---
+++ Lab07/main.cpp
@@ -0,0 +1,32 @@
+// ------------- include header file you will use ------------- //
+#include <iostream>
+#include <string>
+
+// ------------------------------------------------------------ //
+
+using namespace std;
+
+int main()
+{
+	int mode;
+	bool end = false;
+	string str;
+	string text[5], graph[5];
+	text[0] = "(happy)";
+	graph[0] = "^w^";
+	text[1] = "(heart)";
+	graph[1] = "<3";
+	text[2] = "(confused)";
+	graph[2] = "?_?";
+	text[3] = "(kiss)";
+	graph[3] = "-3-";
+	text[4] = "(speechless)";
+	graph[4] = "= =";
+	size_t found;
+
+	// ------------- Coding in here ------------- //
+
+	// ------------------------------------------ //
+
+	return 0;
+}
