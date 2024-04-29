6cd0d84daf9591564ad373c2a2883d31d064a532
awds0318 <brianlin910318@gmail.com>
2024-04-29 18:00:10 +0800

    update Lab08

--- Lab08/main.cpp
+++ Lab08/main.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 using namespace std;
 
-class BubbleSortArray {
+class BubbleSortArray
+{
 
-    private:
-        int *array; // array need to be sorted
-        int size;   // array size
+private:
+    int *array; // array need to be sorted
+    int size;   // array size
 
-    public:
-
-        // (1) constructor
-        // (2) destructors
-        // (3) bubble sort function : void bubbleSort();
-        // (4) display sorted array : void display();
-        // (5) find the max number in array, 
-        //     The function should return " int * " which points to the max number : int* findMax();
-        
+public:
+    // (1) constructor
+    // (2) destructors
+    // (3) bubble sort function : void bubbleSort();
+    // (4) display sorted array : void display();
+    // (5) find the max number in array,
+    //     The function should return " int * " which points to the max number : int* findMax();
 };
 
-
-void BubbleSortArray::bubbleSort() {
-    for (int i = 0; i < size - 1; ++i) {
-        for (int j = 0; j < size - i - 1; ++j) {
-            if (array[j] > array[j + 1]) {
+void BubbleSortArray::bubbleSort()
+{
+    for (int i = 0; i < size - 1; ++i)
+    {
+        for (int j = 0; j < size - i - 1; ++j)
+        {
+            if (array[j] > array[j + 1])
+            {
                 int temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
@@ -32,9 +34,10 @@ void BubbleSortArray::bubbleSort() {
 }
 
 void BubbleSortArray::display() {}
-int* BubbleSortArray::findMax() {}
+int *BubbleSortArray::findMax() {}
 
-int main() {
+int main()
+{
 
     /*     modify this block is not allowed      */
 
@@ -42,27 +45,20 @@ int main() {
     cout << "Enter the size of the array: ";
     cin >> n;
 
-    BubbleSortArray *bubble_ptr ;
+    BubbleSortArray *bubble_ptr;
     BubbleSortArray arr(n);
-    
-    bubble_ptr = &arr ;
+
+    bubble_ptr = &arr;
     int *max_ptr = NULL; // point to the max number in array
 
     /*     modify this block is not allowed      */
 
-
-
     // write your code in the below block
-    // only use "bubble_ptr" and "max_ptr" 
+    // only use "bubble_ptr" and "max_ptr"
     // can not initialize other parameter
     /*                                   */
 
-
-
-
-    
     /*                                   */
 
     return 0;
-
 }
